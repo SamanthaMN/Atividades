e2a21b68e8c4aa3b8d22a4b2b79793b3aed3eac0
Samantha de Matos <55674937+SamanthaMN@users.noreply.github.com>
2024-03-05 22:00:40 -0300

    fix descricao returned

--- CalculoIMC/routes/imc.js
+++ CalculoIMC/routes/imc.js
@@ -7,12 +7,23 @@ router.post('/calculate', function(req, res, next) {
     const altura = req.body.height
 
     const imc = (peso/altura) /altura
+    let descricao = ""
+    
+    if(imc < 18.5){
+        descricao = "Baixo peso"
+    }else if(imc >= 18.5 || imc <= 24.99){
+        descricao = "Normal"
+    }else if(imc >= 25 || imc <= 29.99){
+        descricao = "SobrePeso"
+    }else if(imc > 30){
+        descricao = "Obesidade"
+    }
 
     const bodyReturn = {
         height : altura,
         weight : peso,
         imc : imc,
-        imcDescription : "blabla"
+        imcDescription : descricao
     }
 
     res.send(bodyReturn);
